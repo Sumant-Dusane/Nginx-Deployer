afb326a2862161adbdac7a9d36586b3b7c73fd9f
Sumant-Dusane <sumant.dusane1966@gmail.com>
2025-08-20 02:03:28 +0530

    version change in sh

--- install.sh
+++ install.sh
@@ -2,7 +2,7 @@ set -e
 
 echo "🚀 Installing nginx-deployer CLI..."
 
-URL="https://github.com/Sumant-Dusane/Nginx-Deployer/releases/download/v1.0.0-beta/nginx-deployer_linux_arm_64"
+URL="https://github.com/Sumant-Dusane/Nginx-Deployer/releases/download/v1.0.2-beta/nginx-deployer_linux_arm_64"
 
 curl -fsSL -o nginx-deployer "$URL" && \
 chmod +x nginx-deployer && \
